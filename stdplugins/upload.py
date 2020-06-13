6e90c0b4a1110c6f7d27a8170e624f93f71d3f75
gautamajay52 <25435568+gautamajay52@users.noreply.github.com>
2020-06-14 00:22:30 +0530

    Update upload.py

--- stdplugins/upload.py
+++ stdplugins/upload.py
@@ -25,8 +25,8 @@ thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
 
 def get_lst_of_files(input_directory, output_lst):
     for root, dirs, files in os.walk(input_directory):
-	    for file_name in files:
-		    output_lst.append(os.path.join(root, file_name))
+	for file_name in files:
+            output_lst.append(os.path.join(root, file_name))
     return output_lst
 
 
